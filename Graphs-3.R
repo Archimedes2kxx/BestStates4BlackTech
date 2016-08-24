887429c10118538300924eb1fa29ec338798416b
archimedes2kxx <roylbeasley@gmail.com>
2016-08-24 18:12:17 -0400

    Adding scatter plots and regression lines and regression coefficients

--- Graphs-3.R
+++ Graphs-3.R
@@ -1,1 +1,29 @@
 ### 3. Produce the graphs from the stats created in Stats-2
+###     Specifically, produce plots of simple regressions of Tech share vs. Population shares
+###     for Blacks, Whites, and Asians
+
+### Load stats df and objects
+load(file="stats.RData")
+library(R6)
+load(file="R6Objects.RData")
+
+### Calculate simple regressions of racial percent of tech vs racial percent of total population
+blackParityModel <- lm(blackPerTech ~ blackPerTotPop, dfBlackStats)
+blackParityModel
+whiteParityModel <- lm(whitePerTech ~ whitePerTotPop, dfWhiteStats)
+asianParityModel <- lm(asianPerTech ~ asianPerTotPop, dfAsianStats)
+asianParityModel
+
+### Crude scatterplots
+plot(dfBlackStats$blackPerTotPop, dfBlackStats$blackPerTech)
+abline(blackParityModel)
+
+plot(dfWhiteStats$whitePerTotPop, dfWhiteStats$whitePerTech)
+abline(whiteParityModel)
+
+plot(dfAsianStats$asianPerTotPop, dfAsianStats$asianPerTech)
+abline(asianParityModel)
+
+
+
+
