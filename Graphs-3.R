3ce8546f57d9554038822a5a329dcbbd6bbd1da2
archimedes2kxx <roylbeasley@gmail.com>
2016-08-25 08:49:23 -0400

    Create beta table

--- Graphs-3.R
+++ Graphs-3.R
@@ -7,7 +7,7 @@ load(file="stats.RData")
 library(R6)
 load(file="R6Objects.RData")
 
-### Calculate simple regressions of racial percent of total population vs racial percent of tech
+### A. Calculate simple regressions of racial percent of total population vs racial percent of tech
 blackParityModel <- lm(blackPerTech ~ blackPerTotPop, dfBlackStats)
 blackParityModel
 whiteParityModel <- lm(whitePerTech ~ whitePerTotPop, dfWhiteStats)
@@ -15,7 +15,36 @@ whiteParityModel
 asianParityModel <- lm(asianPerTech ~ asianPerTotPop, dfAsianStats)
 asianParityModel
 
-### Crude scatterplots
+### B. Create table of parity ratios and betas
+matRatiosBetas = matrix(data=NA, nrow=3, ncol=7) 
+blackVec <- c(CA$blackRatio, DC$blackRatio, GA$blackRatio, NY$blackRatio, NC$blackRatio, WA$blackRatio)
+blackVec <- sort(blackVec)
+blackVec <- c(blackVec, blackParityModel$coefficients[2])
+
+whiteVec <- c(CA$blackRatio, DC$whiteRatio, GA$whiteRatio, NY$whiteRatio, NC$whiteRatio, WA$whiteRatio)
+whiteVec <- sort(whiteVec)
+whiteVec <- c(whiteVec, whiteParityModel$coefficients[2])
+
+asianVec <- c(CA$asianRatio, DC$asianRatio, GA$asianRatio, NY$asianRatio, NC$asianRatio, WA$asianRatio)
+asianVec <- sort(asianVec)
+asianVec <- c(asianVec, asianParityModel$coefficients[2])
+
+rownames(matRatiosBetas) <- c("black", "white", "asian")
+matRatiosBetas[1,] <- blackVec
+matRatiosBetas[2,] <- whiteVec
+matRatiosBetas[3,] <- asianVec
+
+matRatiosBetas
+
+
+
+
+
+matRatiosBetas
+
+
+
+### C. Create scatter plots of tech shares vs. population shares plus regression lines w ggplot
 plot(dfBlackStats$blackPerTotPop, dfBlackStats$blackPerTech)
 abline(blackParityModel)
 
