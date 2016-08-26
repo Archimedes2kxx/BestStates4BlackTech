2a27eb4c597715e21fa515dba92c428499374f15
archimedes2kxx <roylbeasley@gmail.com>
2016-08-26 09:04:52 -0400

    More mods for larger data sets

--- Data-1.R
+++ Data-1.R
@@ -39,9 +39,9 @@ stateCodeBook = makeCodeBook(stateCodeLabels)
 occupationCodeBook = makeCodeBook(occupationCodeLabels) 
 
 ### 2. Read data ... read all variables as strings 
-file = "Sex-Race-Hisp-AllOccupations-AllStates-PersonalWeight-PUMA-2014-Data.csv"
+### file = "Sex-Race-Hisp-AllOccupations-AllStates-PersonalWeight-PUMA-2014-Data.csv"
 ### file = "Sex-Race-Hisp-Occupation-State-PersonalWeight-PUMA-2014-Data.txt"
-###file = "Race-CA-DC-GA-NC-NY-WA-Occupation-PUMA-2014.csv"
+file = "Race-CA-DC-GA-NC-NY-WA-Occupation-PUMA-2014.csv"
 census = read.csv(file, header=TRUE, sep=",", stringsAsFactors = FALSE, colClasses = "character")
 ### str(census) ### 45347 obs. of  6 variables for all states
 str(census) ### 3131680 obs. of  6 variables: for all states, all occupations, all races,
@@ -52,6 +52,7 @@ census2 = census
 colnames(census2) = c("race", "state", "occupation")
 
 ### 4. Convert variables to factors with labels from codebook
+### In other words, convert data from integers to category names, e.g., black, California, etc
 ### xCodes are comprehensive dictionaries that contains all codes, not just the ones for this report
 ### Loop through values in each char variable, selecting matching label in xCodes
 ###     Returns factor version with labels on values
@@ -81,7 +82,7 @@ census2$race = createFactorsWithLabels(census2$race, raceCodeBook)
 census2$state = sprintf("%03s", census2$state) ### states are 3-digit codes, some with leading zeros
 census2$state = createFactorsWithLabels(census2$state, stateCodeBook)
 census2$occupation = createFactorsWithLabels(census2$occupation, occupationCodeBook)
-
+head(census2)
 numSampleObservations = dim(census2)[1]
 
 ### 5. Save census2 df into file
