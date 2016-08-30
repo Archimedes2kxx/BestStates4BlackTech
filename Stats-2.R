2d1ef90d5ca49785b2dfbb49c216ef36c7cfa13e
archimedes2kxx <roylbeasley@gmail.com>
2016-08-29 23:16:31 -0400

    Stats-2 will accept data from Data-1 module, create state R6 objects, store all AFF parameters in the objects, compute statistics from the sample data and store them in the sample; and compute hybrid statistics based on parameters and sample statistics.

--- Stats-2.R
+++ Stats-2.R
@@ -1,5 +1,10 @@
 setwd("/Users/roylbeasley/Google Drive/Diversity/Census-Bureau/BestStates4BlackTech")
 
+###install.packages("R6")
+library(R6) 
+
+load(file = "dfStates3.RData")
+
 #####
 ### LOGIC -- Create 51 state objects using W. Chang's R6 class
 ###     Real paramaters of states are provided to constructor ("initialize") function
@@ -7,107 +12,9 @@ setwd("/Users/roylbeasley/Google Drive/Diversity/Census-Bureau/BestStates4BlackT
 ###     Other statistics are based on dat frame statistics and parameters
 ###     Calculated stats become estimates for the corresponding population parameters 
 
-###     Parameters for first study = total population, Black, White, Asian, Hispanic, and "other"
-###     Gender will not be included in the first study
-
-###install.packages("R6")
-library(R6) 
-
-### A. Read American Fact Finder (AFF) racial parameters for population of states (and DC) in 2014
-### American Fact finder URL ... table PEPSR6H
-### http://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=PEP_2015_PEPSR6H&prodType=table
-file = "PEP_2014_PEPSR6H_with_ann.csv"
-statesPop <- read.csv(file, header=FALSE, stringsAsFactors = FALSE)
-str(statesPop)
-head(statesPop,5)
-
-### Get var names from second row
-varNames <- as.vector(unlist(statesPop[1,]))
-colnames(statesPop) <- varNames
-str(statesPop)
-
-### Drop display labels and var names in 1st, 2nd rows
-statesPop <- statesPop[c(-1, -2),]
-str(statesPop)
-head(statesPop)
-
-### 1. Get data for all non-hispanics
-dfNonHispanics <- subset(statesPop, Year.id=="est72014" & Sex.id =="totsex"& Hisp.id=="nhisp", 
-                    select=c(wa:tom))
-dfNonHispanics$totpop <- NULL
-
-### 2. Get data for hispanics
-dfHispanics <- subset(statesPop, Year.id=="est72014" & Sex.id =="totsex"& Hisp.id=="hisp", 
-                    select=c(totpop))
-dfHispanics$hisp <- dfHispanics$totpop
-dfHispanics$totpop <- NULL
-
-### 3. Get total pop of the state
-dfTotPop <- subset(statesPop, Year.id=="est72014" & Sex.id=="totsex" & Hisp.id=="tothisp", 
-                      select=c(`GEO.display-label`, totpop))
-
-### 4. cbind the columns
-dfStates1 <- cbind(dfTotPop, dfNonHispanics,dfHispanics)
-str(dfStates1)
-
-### 5. Confer final names on all variables
-columnNames <- c("state", "totpop", "white", "black", "amInAlNat", "asian", "pacific", "mixed" , "hisp")
-colnames(dfStates1) <- columnNames
-str(dfStates1)
-
-### 6. Convert state to factor ... useful for tapply functions and other stuff
-dfStates2 <- dfStates1
-dfStates2$state <- as.factor(dfStates2$state)
-str(dfStates2[,1])
-
-### 7. Convert data columns to integers
-vec <- as.integer(unlist(dfStates2[,2:9]))
-mat <- matrix(vec, nrow=51, ncol=8)
-dfStates2 <- data.frame(dfStates2[,1], mat)
-colnames(dfStates2) <- columnNames ### ... yes, again
-str(dfStates2)
-head(dfStates2)
-
-### 8. Add row names = states .... convenient for subsetting with brackets
-rownames(dfStates2) <- dfStates2$state
-head(dfStates2)
-
-### 9 . Combine races that will not be analyzed, then delete their columns
-dfStates2$other <- dfStates2$amInAlNat + dfStates2$pacific + dfStates2$mixed
-dfStates2$amInAlNat <- NULL
-dfStates2$pacific <- NULL
-dfStates2$mixed <- NULL
-str(dfStates2)
-
-### 10. Calculate derived parameters ... percentage of each racial group in the total population
-dfStates3 <- dfStates2
-str(dfStates3)
-dfStates3$perWhite <- round(dfStates3$white / dfStates3$totpop, digits = 3)
-dfStates3$perBlack <- round(dfStates3$black / dfStates3$totpop, digits = 3)
-dfStates3$perAsian <- round(dfStates3$asian / dfStates3$totpop, digits = 3)
-dfStates3$perHisp <- round(dfStates3$hisp / dfStates3$totpop, digits = 3)
-dfStates3$perOther <- round(dfStates3$other / dfStates3$totpop, digits = 3)
-str(dfStates3)
-
-save(dfStates3, file = "dfStates3.RData")
-
-### 11. Examples of state parameters
-dfStates3["California",]
-dfStates3["New York",]
-dfStates3["Georgia",]
-dfStates3["District of Columbia",]
-dfStates3["Washington",]
-dfStates3["Hawaii",]
-dfStates3["Pennsylvania",]
-dfStates3["Oregon",]
-
-### 12. Consistency checks ... to sum of populations of groups add up to state total pop???
-
-
-
 
 #################################
-##### B. Create state objects and set sample stats, parameters, and hybrid stats
+##### A. Create state objects and set sample stats, parameters, and hybrid stats
 load("census2.RData")
 numSampleObservations = dim(census2)[1]
 
