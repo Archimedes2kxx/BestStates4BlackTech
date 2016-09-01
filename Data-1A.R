04630dbde317dc7f8d5d9df3d0a82b2cc243a34e
archimedes2kxx <roylbeasley@gmail.com>
2016-09-01 11:35:17 -0400

    Add summary stats about total employment in info tech, racial totals, racial shares

--- Data-1A.R
+++ Data-1A.R
@@ -149,7 +149,7 @@ save(dfRaceSharesPerState, file="dfRaceSharesPerState.RData")
 (allRacesInTech <- colSums(dfRaceEmploymentPerState[,2:6]))
 (allTech <- sum(allRacesInTech)) ### 4125164
 (raceSharesInTech <- round((allRacesInTech/allTech), digits=3))
-dfTechShares <- data_frame(allRacesInTech, allTech, racialSharesInTech)
+dfTechShares <- data.frame(allRacesInTech, allTech, raceSharesInTech)
 save(dfTechShares, file="dfTechShares.RData")
-
+  
 
