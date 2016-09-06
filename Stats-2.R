014376ecf3446077ce1dd1017b8888759840a631
archimedes2kxx <roylbeasley@gmail.com>
2016-09-06 15:53:24 -0400

    Revised plan in comments about tables. Only Tables 1.1, 1.2, 1.3 and Tables 2B, 2W, 2A, 2H ... and maps ... and regression lines

--- Stats-2.R
+++ Stats-2.R
@@ -62,41 +62,19 @@ index <- order(dfOccupation$Employees, decreasing=TRUE)
 dfOccupation[index,]
 
 
-
-### Tables 2A, 2B, 2C, 2D. How many members of racial groups  in each state ... totals, white, ### black, asian, hispanic and percents racial shares of total population in each state
-### ... sorted in decreasing order for racial groups ... so users can see "Top 10"
+### Tables 2A, 2B, 2C, 2D. Racial groups in each state  
+### ... state, <racial>TechEmp, totalTechEmp, per<Racial>TechEmp, totPop, <racialPop>, per<Racial>Pop, <racial>Parity ... bottom row shows racial betas described below
+### ... sorted by decreasing racialTechEmp so users can see "Top 10"
 ### ... Only show top 10 in report, show full tables 2AA, 2BB, 2CC, 2DD in appendices on GitHub
+
+dfEmploymentAndShares
 dfStatesPop3
 
-### Maps 2W, 2B, 2A, 2H ... maps of white, black, asian, hispanics in states.
+### Maps 2W, 2B, 2A, 2H ... maps of white, black, asian, hispanics in tech employment states.
 ### Maps 2.1W, 2.1B, 2.1A, 2.1H ... maps of % white, black, asian, hispanics in states.
 
-### Tables 3W, 3B, 3A, 3H ... How many techs in each state and shares (%) of tech employmnet 
-### for white, black, asian, and hispanic techs sorted in order of decreasing tech employment for 
-### each total pop in the state .... allow identification of "top 10" in terms of most number of 
-### minority employees .. only show top 10; full tables 3WW, 3BB, 3AA, 3HH in appendices on GitHub
-dfEmploymentAndShares
-
-### Maps 3W, 3B, 3A, 3H ... maps of white, black, asian, hispanic techs in each state
-### Maps 3.1W, 3.1B, 3.1A, 3.1H ... maps of  white, black, asian, hispanic % of techs in each state
-
-
-### Tables 4W, 4B, 4A, 4H ... parity ratios, i.e., tech share/populaton shares for white, black, 
-### asian, and hispanic ratios sortin states in decreasing order for each groups... only show
-### top ten ...  full 4WW, 4BB, 4AA, 4HH in appendices on GitHub
-
-### Maps 4W, 4B, 4A, 4H ... maps of parity ratios for each racial group
-
-
-### Tables 5W, 5B, 5A, 5H ... regressions of Betas for racial shares of tech in each state vs. white
-### blacks, asians, and hispanic population in each state ... show that a 1 percent increase in 
-### share of pop yields an X percent increase in share of tech employment ... sorted in order of 
-###decreasing size of ratios ... enable picking the 10 fairest states 
-### ... only show top 10;  full 5WW, 5BB, 5AA, 5HH in appendices on GitHub
-
-### Plots 5W, 5B, 5A, 5H ... regression lines for racial component ot each state's tech sector vs. ### the racial component of each state's population. The Beta slopes are in Tables 5A, 5B, 5C, 5D 
-### ... ALL ON THE SAME PLOT FRAME so user can see that asian slope is much steeper than white, 
-### black, and hispanic ... Betas show that a 1 percent increase in a group's population yields a 
-### Beta percent increase in the size of the group's tech employment
+### Plots 2W, 2B, 2A, 2H ... regression lines for racial component ot each state's tech sector vs. ### the racial component of each state's population. The Beta slopes are in Tables 2A, 2B, 2C, 2D 
+### ... ALL ON THE SAME PLOT FRAME so user can see that asian slope is much 
+### steeper than white, black, and hispanic 
 
 
